fafdab439e864ff3f110c5261ac72e9fe16e0bcc
Ddayin <ddai.develop@gmail.com>
2024-11-21 17:39:50 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyCheckBox(),
+    );
+  }
+}
+
+class MyCheckBox extends StatefulWidget {
+  const MyCheckBox({super.key});
+
+  @override
+  State<MyCheckBox> createState() => _MyCheckBoxState();
+}
+
+class _MyCheckBoxState extends State<MyCheckBox> {
+  bool isChecked = false;
+
+  void _toggleCheckbox() {
+    setState(() {
+      //isChekedを反転
+      isChecked = !isChecked;
+    });
+    print('isChecked = ${isChecked}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyCheckBox Demo'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 70),
+              child: CheckboxListTile(
+                controlAffinity: ListTileControlAffinity.leading,
+                title: const Text('利用規約に同意する'),
+                value: isChecked,
+                onChanged: (value){
+                  _toggleCheckbox();
+                }
+              ),
+            ),
+            Text('isChecked = ${isChecked}')
+          ],
+        ),
+      ),
+    );
+  }
+}
